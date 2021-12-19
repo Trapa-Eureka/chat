c33fccbf28d3909bcb47d797113bbd96a14ed991
Trapa-Eureka <son199@hanmail.net>
2021-12-19 21:35:10 +0900

    sort between Message and Nickname #1

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Zoom clone using NodeJS WebRTC and WebSockets
 - front, backend에서 websocket의 real-time 역할
 - Websocket에서 front-end가 back-end로 real-time으로 뭔가 보낼 수 있는 방법이 있다(반대의 경우도 당연히 가능)
 - 서로다른 브라우저들 사이에도 대화가 오고갈 수 있게끔
+- 메시지를 구별해주는 방법. 타입별 설정. (msg chat type, nickname type)
+- JS object를 가지고 string으로 만드는 가장 좋은 방법
+- 이 string을 다시 JS object로 만드는 방법
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,23 @@
-const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 // 어디에 있는지 알려주는 것 (같은 url)
 // app.js socket : 서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 function handleOpen() {
     console.log("connected with Server");
 }
 function handleMessage(message) {
-    console.log("New message : ", message.data);
+    // console.log("New message : ", message.data);
+    const li = document.createElement("li"); // create li when receive new message
+    li.innerText = message.data; // li에 메세지를 출력
+    messageList.append(li); // messageList에 li를 추가
 }
 function handleDisconnect() {
     console.log("disconnected from Server");
@@ -22,12 +31,15 @@ socket.addEventListener("close", handleDisconnect);
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
     input.value = "";
-    // const input = event.target.querySelector('input');
-    // const value = input.value;
-    // input.value = "";
-    // socket.send(value);
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
